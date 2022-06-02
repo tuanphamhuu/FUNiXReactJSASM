984d3542eb1e197b0e063289544859d388269895
tuanphamhuu <96778307+tuanphamhuu@users.noreply.github.com>
2022-06-02 19:33:56 +0900

    validate ...

--- src/Components/AddStaffsComponent.js
+++ src/Components/AddStaffsComponent.js
@@ -1,123 +1,243 @@
 import React, { Component } from "react";
 import {
-    Button, Modal, ModalHeader, ModalBody,
-    Form, FormGroup, Input, Label, Col
-} from 'reactstrap';
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Form,
+  FormGroup,
+  Input,
+  Label,
+  Col,
+  FormFeedback,
+} from "reactstrap";
 
 class AddStaffs extends Component {
-    //    tạo state cho Component Header 
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: '',
-            doB: '',
-            startDate: '',
-            salaryScale: '',
-            department: '',
-            annualLeave: '',
-            overTime: '',
-            salary: '',
-            image: '/assets/images/alberto.png',
+  //    tạo state cho Component Header
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      doB: "",
+      startDate: "",
+      salaryScale: "",
+      department: "",
+      annualLeave: "",
+      overTime: "",
+      salary: "",
+      image: "/assets/images/alberto.png",
+      isModalOpen: false,
+      touched: {
+        name: false,
+        doB: false,
+        startDate: false,
+        salaryScale: false,
+        annualLeave: false,
+        overTime: false,
+      },
+    };
+    this.toggleModal = this.toggleModal.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-            isModalOpen: false
-        };
-        this.toggleModal = this.toggleModal.bind(this);
-        this.handleInputChange = this.handleInputChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+  toggleModal() {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen,
+    });
+  }
+  // nhận event làm tham số
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name;
 
-    toggleModal() {
-        this.setState({
-            isModalOpen: !this.state.isModalOpen
-        });
-    }
-// nhận event làm tham số
-    handleInputChange(event) {
-        const target = event.target;
-        const value = target.type === 'checkbox' ? target.checked : target.value;
-        const name = target.name;
+    this.setState({
+      [name]: value,
+    });
+  }
 
-        this.setState({
-            [name]: value
-        });
-    }
+  handleSubmit(event) {
+    console.log("Current State is: " + JSON.stringify(this.state));
+    alert("Current State is: " + JSON.stringify(this.state));
+    event.preventDefault();
+  }
 
-    handleSubmit(event) {
-        console.log('Current State is: ' + JSON.stringify(this.state));
-        alert('Current State is: ' + JSON.stringify(this.state));
-        event.preventDefault();
+  handleBlur = (field) => (evt) => {
+    this.setState({
+      touched: { ...this.state.touched, [field]: true },
+    });
+  };
+
+  validate(name, salaryScale, annualLeave, overTime) {
+    const errors = {
+      name: "",
+      doB: "",
+      startDate: "",
+      salaryScale: "",
+      annualLeave: "",
+      overTime: "",
+    };
+
+    if (this.state.touched.name && name.length < 3)
+      errors.name = "Tên nhân viên phải trên 3 ký tự";
+    else if (this.state.touched.name && name.length > 20)
+      errors.name = "Tên nhân viên phải bé hơn 20 ký tự";
+
+    if (
+      name == null ||
+      name == "" ||
+      doB == null ||
+      doB == "" ||
+      startDate == null ||
+      startDate == "" ||
+      salaryScale == null ||
+      salaryScale == ""
+    ) {
+      errors.name = "Xin hãy nhập thông tin ";
+
+      // const thisYear = new Date().getFullYear();
+      //     if (parseInt(startDate) > parseInt(thisYear)) {
+      //         errors.startDate = ' Ngày vào công ty bị sai';
+      //     }
+      const reg = /^\d+$/;
+      if (this.state.touched.salaryScale && !reg.test(salaryScale))
+        errors.salaryScale = "Tel. Number should contain only numbers";
+      return errors;
     }
-    render() {
-        return (
-            <React.Fragment>
-                    <div >
-                            {/* Icon  Login  Button */}
-                                    <Button color="primary" outline onClick={this.toggleModal}><span className="fa fa-plus fa-lg " ></span></Button>
-                   </div>
-              
-                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
-                    <ModalHeader toggle={this.toggleModal}> Thêm nhân viên</ModalHeader>
-                    {/* Adding the controlled Form - thêm nhân viên */}
-                    <ModalBody>
-                        <Form onSubmit={this.handleSubmit}>
-                            <FormGroup>
-                                <Label htmlFor="name"> Tên </Label>
-                                <Input type="text" id="staffsname" name="staffsname"
-                                        placeholder="Tên nhân viên"
-                                        value={this.state.staffsname}
-                                        onChange={this.handleInputChange} />
-                            </FormGroup>
-                            <FormGroup>
-                                <Label htmlFor="doB"> Ngày sinh</Label>
-                                <Input type="date" id="doB" name="doB"
-                                        value={this.state.doB}
-                                        onChange={this.handleInputChange} />
-                            </FormGroup>
-                            <FormGroup>
-                                <Label htmlFor="startDate"> Ngày vào công ty</Label>
-                                <Input type="date" id="startDate" name="startDate"
-                                    value={this.state.startDate}
-                                    onChange={this.handleInputChange} />
-                            </FormGroup>
-                            <FormGroup >
-                            <Col md={{ size: 3, offset: 0 }}>
-                                <Label htmlFor="department"> Phòng ban </Label><br />
-                                <select id="department" name="department" 
-                                    value={this.state.department}
-                                    onChange={this.handleInputChange} >
-                                    <option value="Sale">Sale</option>
-                                    <option value="HR">HR</option>
-                                    <option value="Marketing">Marketing</option>
-                                    <option value="IT">IT</option>
-                                    <option value="Finance">Finance</option>
-                                </select>
-                                </Col>
-                            </FormGroup>
-                            <FormGroup>
-                                <Label htmlFor="salaryScale"> Hệ số lương </Label>
-                                <Input type="text" id="salaryScale" name="salaryScale"
-                                    value={this.state.salarySale}
-                                    onChange={this.handleInputChange} />
-                            </FormGroup>
-                            <FormGroup>
-                                <Label htmlFor="annualLeave"> Số ngày nghỉ còn lại  </Label>
-                                <Input type="text" id="annualLeave" name="annualLeave"
-                                    value={this.state.annualLeave}
-                                    onChange={this.handleInputChange} />
-                            </FormGroup>
-                            <FormGroup>
-                                <Label htmlFor="overTime"> Số ngày đã làm thêm </Label>
-                                <Input type="text" id="overTime" name="overTime"
-                                    value={this.state.overTime}
-                                    onChange={this.handleInputChange} />
-                            </FormGroup>
-                            <Button type="submit" value="submit" color="primary"> Thêm</Button>
-                        </Form>
-                    </ModalBody>
-                </Modal>
-            </React.Fragment>
-        );
-    }
+  }
+  render() {
+    const errors = this.validate(
+      this.state.name,
+      this.state.doB,
+      this.state.startDate,
+      this.state.salaryScale,
+      this.state.annualLeave,
+      this.state.overTime
+    );
+    return (
+      <React.Fragment>
+        <div>
+          {/* Icon  Login  Button */}
+          <Button color="primary" outline onClick={this.toggleModal}>
+            <span className="fa fa-plus fa-lg "></span>
+          </Button>
+        </div>
+
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+          <ModalHeader toggle={this.toggleModal}> Thêm nhân viên</ModalHeader>
+          {/* Adding the controlled Form - thêm nhân viên */}
+          <ModalBody>
+            <Form onSubmit={this.handleSubmit}>
+              <FormGroup>
+                <Label htmlFor="name"> Tên </Label>
+                <Input
+                  type="text"
+                  id="name"
+                  name="name"
+                  placeholder="Tên nhân viên"
+                  value={this.state.name}
+                  valid={errors.name === ""}
+                  invalid={errors.name !== ""}
+                  onBlur={this.handleBlur("name")}
+                  onChange={this.handleInputChange}
+                />
+                <FormFeedback> {errors.name} </FormFeedback>
+              </FormGroup>
+              <FormGroup>
+                <Label htmlFor="doB"> Ngày sinh</Label>
+                <Input
+                  type="date"
+                  id="doB"
+                  name="doB"
+                  value={this.state.doB}
+                  valid={errors.doB === ""}
+                  invalid={errors.doB !== ""}
+                  onBlur={this.handleBlur("doB")}
+                  onChange={this.handleInputChange}
+                />
+                <FormFeedback> {errors.doB} </FormFeedback>
+              </FormGroup>
+              <FormGroup>
+                <Label htmlFor="startDate"> Ngày vào công ty</Label>
+                <Input
+                  type="date"
+                  id="startDate"
+                  name="startDate"
+                  value={this.state.startDate}
+                  valid={errors.startDate === ""}
+                  invalid={errors.startDate !== ""}
+                  onBlur={this.handleBlur("startDate")}
+                  onChange={this.handleInputChange}
+                />
+                <FormFeedback> {errors.startDate} </FormFeedback>
+              </FormGroup>
+              <FormGroup>
+                <Col md={{ size: 3, offset: 0 }}>
+                  <Label htmlFor="department"> Phòng ban </Label>
+                  <br />
+                  <select
+                    id="department"
+                    name="department"
+                    value={this.state.department}
+                    onChange={this.handleInputChange}
+                  >
+                    <option value="Sale">Sale</option>
+                    <option value="HR">HR</option>
+                    <option value="Marketing">Marketing</option>
+                    <option value="IT">IT</option>
+                    <option value="Finance">Finance</option>
+                  </select>
+                </Col>
+              </FormGroup>
+              <FormGroup>
+                <Label htmlFor="salaryScale"> Hệ số lương </Label>
+                <Input
+                  type="number"
+                  id="salaryScale"
+                  name="salaryScale"
+                  value={this.state.salarySale}
+                  valid={errors.salaryScale === ""}
+                  invalid={errors.salaryScale !== ""}
+                  onBlur={this.handleBlur("salaryScale")}
+                  onChange={this.handleInputChange}
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label htmlFor="annualLeave"> Số ngày nghỉ còn lại </Label>
+                <Input
+                  type="number"
+                  id="annualLeave"
+                  name="annualLeave"
+                  value={this.state.annualLeave}
+                  valid={errors.annualLeave === ""}
+                  invalid={errors.annualLeave !== ""}
+                  onBlur={this.handleBlur("annualLeave")}
+                  onChange={this.handleInputChange}
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label htmlFor="overTime"> Số ngày đã làm thêm </Label>
+                <Input
+                  type="number"
+                  id="overTime"
+                  name="overTime"
+                  value={this.state.overTime}
+                  valid={errors.overTime === ""}
+                  invalid={errors.overTime !== ""}
+                  onBlur={this.handleBlur("overTime")}
+                  onChange={this.handleInputChange}
+                />
+              </FormGroup>
+              <Button type="submit" value="submit" color="primary">
+                {" "}
+                Thêm
+              </Button>
+            </Form>
+          </ModalBody>
+        </Modal>
+      </React.Fragment>
+    );
+  }
 }
-
 export default AddStaffs;
--- src/Components/HeaderComponent.js
+++ src/Components/HeaderComponent.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { NavbarBrand } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {MDBIcon} from "mdb-react-ui-kit";
 function Header() {
 
